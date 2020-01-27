712dc69268b7bb25bbc62bf56b54ef9f8005d9de
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2020-01-27 16:04:30 +0100

    comments

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$directory = './img';
-$fileList = scandir($directory);
-$fileList = array_diff($fileList, Array('.','..'));
+$directory = './img'; //folder na zdjęcia
+$fileList = scandir($directory); //znajdz wszystko w folderze
+$fileList = array_diff($fileList, Array('.','..'));// usuń . i .. (ten katalog i katalog wyżej)
 
+//sprawdzamy co dostaliśmy - do usunięcia
 echo '<pre>';
 print_r($fileList);
+
 ?>
